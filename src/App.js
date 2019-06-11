3c0d92218db0612bc28d2ea57c8235a70aa7b638
pawellysy <pawellysy96@gmail.com>
2019-06-11 12:34:26 +0200

    displaying works

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import './App.css';
 import { tsIndexSignature } from '@babel/types';
-import { __values } from 'tslib';
+import { __values } from 'tslib'
 import DatasetsTable from './components/DatasetsTable'
+
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 650,
+  },
+}));
+
+
 
 
 function loadScript (url) {
@@ -23,14 +45,8 @@ function loadScript (url) {
     constructor(props){
       super(props); 
       this.state = {
-        position:{
-        lat: 50.052453,
-        lng:  19.941800
-        },
-        searchPlace: '',
-        data: [
-          {name: "Krakow", lat: 50.052453,lng:  19.941800}
-        ]
+      searchPlace: '',
+      data: []
       }
     }
     
@@ -46,7 +62,7 @@ function loadScript (url) {
 
      initMap = () => {
      this.map = new window.google.maps.Map(document.getElementById('map'), {
-        center: {lat: this.state.position.lat, lng: this.state.position.lng},
+        center: {lat: 50, lng: 10},
         zoom: 13
        
       });
@@ -56,16 +72,17 @@ function loadScript (url) {
         position: this.state.position
       });
 
-      var infoWindow = new window.google.maps.InfoWindow();
+      var infowindow = new window.google.maps.InfoWindow();
       var infowindowContent = document.getElementById("infowindow")
-      infoWindow.setContent(infowindowContent)
+      infowindow.setContent(infowindowContent)
      
 
       
       this.autocomplete = new window.google.maps.places.Autocomplete( document.getElementById("pac-input"));
       this.autocomplete.addListener('place_changed', () => {
       var place = this.autocomplete.getPlace();
-      infoWindow.close();
+      this.places = place;
+      infowindow.close();
       this.marker.setVisible(false);
       if (!place.geometry) {
         // User entered the name of a Place that was not suggested and
@@ -73,39 +90,40 @@ function loadScript (url) {
         window.alert("No details available for input: '" + place.name + "'");
         return;
       }
-  
+      var data = this.state.data;
+      var info = {
+        id: place.id,
+        address: place.formatted_address,
+        location: place.geometry.location
+        //  place.geometry.location
+        }
+
+
+      data.push({info});
+      console.log(info)
+      this.setState({data: data});
+      
       // If the place has a geometry, then present it on a map.
       if (place.geometry.viewport) {
         this.map.fitBounds(place.geometry.viewport);
       } else {
         this.map.setCenter(place.geometry.location);
-        this.map.setZoom(17);  // Why 17? Because it looks good.
+        this.map.setZoom(17); 
+       
       }
       this.marker.setPosition(place.geometry.location);
       this.marker.setVisible(true);
-      
-
-      
-      
-
 
       })
-      
-     
+
     }
     
-        
-        
     
-
-   
-
     render(){
-     console.log(this.state.searchPlace)
+     
+      console.log(this.state);
     return (
       
-
-
       <div className="App">
        
         <div>
@@ -121,10 +139,31 @@ function loadScript (url) {
   <span id="place-address"></span>
 </div>
 
-        
-
         <div id="table">
-          <DatasetsTable dataset = {this.state.data}/>
+          <Paper>
+          <Table >
+        <TableHead>
+          <TableRow>
+            <TableCell>  Name of the place Place  </TableCell>
+            <TableCell align="right">Latitude</TableCell>
+            <TableCell align="right">Longitude</TableCell>
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          {this.state.data.map(place => (
+       
+            <TableRow key={place.info.id}>
+              <TableCell component="th" scope="row">{place.info.address}   </TableCell>
+              <TableCell align="right">             {place.info.location.lat() } </TableCell>
+              <TableCell align="right">             {place.info.location.lng()}</TableCell>
+            </TableRow>
+          
+          ))}
+        </TableBody>
+        </Table>
+        </Paper>
+        <DatasetsTable dataset = {this.state.data}/>
           
 
         </div>        
